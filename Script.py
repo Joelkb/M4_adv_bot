6fd74af1568477eb47349fc2e5f4c9a96e9eb0ee
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2021-12-27 15:15:05 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -21,7 +21,7 @@ class script(object):
 - Filter is the feature were users can set automated replies for a particular keyword and {} will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. {} should have admin privillage.
+1. This bot should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -32,11 +32,11 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- {} Supports both url and alert inline buttons.
+- This bot Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. {} supports buttons with any telegram media type.
+2. This bot supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
@@ -67,7 +67,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of {}
+These are the extra features of this bot
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specifed user.</code>
