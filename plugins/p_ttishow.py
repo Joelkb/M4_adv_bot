d654fdec179fae55f8e4bab82e7c2fc4f6080911
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-01-20 10:55:22 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,8 +37,8 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('🍿 Movie Request Group🍿', url='https://t.me/+TuzhGj7SC871arDV')
+            InlineKeyboardButton('ℹ️ ʜᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('🍿ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ🍿', url='https://t.me/+TuzhGj7SC871arDV')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
