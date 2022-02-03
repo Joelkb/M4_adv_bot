07882e034ee3dbcf8b0c7cd81db28e26db8d1298
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-02-03 15:03:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+TuzhGj7SC871arDV')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'filmy_harbour_support')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
